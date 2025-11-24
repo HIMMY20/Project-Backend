7187ebd821ab593aad26c7ada7984fd994f486a8
HIMMY20 <chauhanmhimanshu2005@gmail.com>
2025-11-25 00:18:52 +0530

    fix mongo url

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -3,7 +3,10 @@ let mg = require("mongoose")
 let connection = async()=>{
     try {
         // await mg.connect("mongodb://localhost:27017/Lost")
-        await mg.connect("mongodb+srv://himanshu2005_db_user:Khushieee1407@cluster0.ooka1sp.mongodb.net/Lost?appName=Cluster0")
+await mongoose.connect(
+  "mongodb+srv://himanshu2005_db_user:Khushieee1407@cluster0.ooka1sp.mongodb.net/Lost?retryWrites=true&w=majority&appName=Cluster0"
+);
+
         console.log("Database Connnected")
     } catch (error) {
         console.log(error)
